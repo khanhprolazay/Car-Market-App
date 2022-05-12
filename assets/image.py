75ab936771fe7d4cb873e667aed5f38382bf8e1f
Le Dang Minh <khanhprolazay@gmail.com>
2022-05-12 18:22:25 +0700

    UPDATE assets

---
+++ assets/image.py
@@ -0,0 +1,39 @@
+logo = {
+    "KIA": "assets/logo/kia.png",
+    "Hyundai": "assets/logo/huyndai.jpg",
+    "Toyota": "assets/logo/toyota.jpg",
+    "Ford": "assets/logo/ford.jpg",
+    "Honda": "assets/logo/honda.jpg",
+    "Mazda": "assets/logo/mazda.png",
+    "Chevrolet": "assets/logo/chervolet.jpg",
+    "Volkswagen": "assets/logo/volkswagen.jpg",
+    "BMW": "assets/logo/bmw.jpg",
+    "Mercedes-Benz": "assets/logo/mercedes-benz.jpg",
+    "Peugeot": "assets/logo/peugeot.jpg",
+    "Suzuki": "assets/logo/suzuki.jpg",
+    "Nissan": "assets/logo/nissan.jpg",
+    "Mitsubishi": "assets/logo/mitsubishi.jpg",
+    "Isuzu": "assets/logo/isuzu.png",
+    "THACO": "assets/logo/thaco.jpg",
+    "Hino": "assets/logo/hino.png",
+    "Lexus": "assets/logo/lexus.jpg",
+    "Land Rover": "assets/logo/landrover.png",
+    "Subaru": "assets/logo/subaru.jpg",
+    "Jaguar": "assets/logo/jaguar.jpg",
+    "Audi": "assets/logo/audi.jpg",
+    "Rolls-Royce": "assets/logo/rollsroyce.png",
+    "Renault": "assets/logo/renault.jpg",
+    "Bentley": "assets/logo/bentley.jpg",
+    "Porsche": "assets/logo/porsche.jpg",
+    "Volvo": "assets/logo/volvo.jpg"
+}
+
+type = {
+    "Couple": "assets/type/couple.jpg",
+    "Hatchback": "assets/type/hatchback.png",
+    "Minivan": "assets/type/minivan.png",
+    "Pick-up": "assets/type/pickup.png",
+    "Sedan": "assets/type/sedan.jpg",
+    "SUV": "assets/type/suv.png",
+    "Van": "assets/type/van.png"
+}
